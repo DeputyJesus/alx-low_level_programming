bf703da882ef981d81fc36b539c7807135e08042
DeputyJesus <enpascalelijah@gmail.com>
2023-07-28 15:29:43 -0700

    two files edited

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,10 @@
-section .data
-    format db 'Hello, Holberton', 0
 
-section .text
-    global main
-    extern printf
-
+       global    main
+          extern    printf
 main:
-    mov edi, format     ; Load the address of the format string into the EDI register
-    xor eax, eax        ; Clear the EAX register (set it to 0)
-    call printf         ; Call the printf function to print the formatted string
-    mov eax, 0          ; Set the return value to 0
-    ret                 ; Return from the main function
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
