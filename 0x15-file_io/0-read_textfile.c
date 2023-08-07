55fb711fb44d3bbdc2fcbc287b0043cab1bf949e
DeputyJesus <enpascalelijah@gmail.com>
2023-08-07 15:19:11 -0700

    all task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file being read.
- * @letters: Number of letters to be read.
+ * @filename: The text file being read.
+ * @letters: The number of letters to be read.
  *
- * Return: The actual number of bytes read and printed,
+ * Return: The actual number of bytes read and printed.
  *         0 when the function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t w;
-int fd;
-char *buf;
+	char *buf;
+	ssize_t fd, w, t;
 
-if (filename == NULL)
-return (0);
+	if (filename == NULL)
+		return (0);
 
-/*
- * Open file in read-only mode
- */
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-/*
- * Allocate memory for the buffer
- */
-buf = malloc(sizeof(char) * letters);
-if (buf == NULL)
-{
-close(fd);
-return (0);
-}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-/*
- * * Read from the file into the buffer
- */
-ssize_t t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
+	t = read(fd, buf, letters);
+	if (t == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-/*
- * Free the buffer and close the file descriptor
- */
-free(buf);
-close(fd);
+	w = write(STDOUT_FILENO, buf, t);
+	if (w == -1 || w != t)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-return (w);
+	free(buf);
+	close(fd);
+
+	return (w);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -71,11 +71,11 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while (r > 0)
-	{
+	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -83,14 +83,16 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-	}
+
+	} while (r > 0);
 
 	free(buffer);
 	close_file(from);
